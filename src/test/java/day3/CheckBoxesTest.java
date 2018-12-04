4d6a1ca109799ecc9f909792880fc3f961b612c2
oleksandrUtah <ste.kozachenko@outlook.com>
2018-12-04 13:17:36 -0700

    12/4/2018

--- src/test/java/day3/CheckBoxesTest.java
+++ src/test/java/day3/CheckBoxesTest.java
@@ -7,15 +7,11 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
-
 import java.util.concurrent.TimeUnit;
 
 public class CheckBoxesTest {
-
     WebDriver driver;
-
     By checkBox1 = By.cssSelector("#checkboxes > input:nth-child(1)");
-
 
     @BeforeSuite
     public void suiteSetup(){
@@ -23,8 +19,6 @@ public class CheckBoxesTest {
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
     }
-
-
     @Test
     public void testCheckBoxes() {
         String windowsPageURL = "http://the-internet.herokuapp.com/checkboxes";
@@ -33,21 +27,23 @@ public class CheckBoxesTest {
         clickOnFirstCheckBox();
         assertCheckBoxChecked();
     }
-
     private void assertCheckBoxChecked() {
         WebElement checkBoxElement = driver.findElement(checkBox1);
         Boolean isSelected = checkBoxElement.isSelected();
         String checkedAttribute = checkBoxElement.getAttribute("checked");
 
         Assert.assertNotNull(checkedAttribute);
-        Assert.assertTrue(checkedAttribute.equals("true"));
-        Assert.assertTrue(isSelected);
-    }
+        // 1-st assert used for checking if the "checked" attribute is even exist
 
+        Assert.assertTrue(checkedAttribute.equals("true"));
+        // 2-nd assert is check for the value of that "checked" attribute
+
+        Assert.assertTrue(isSelected);
+        //3-d assert is using method isSelected()
+    }
     private void clickOnFirstCheckBox() {
         driver.findElement(checkBox1).click();
     }
-
     private void navigateToURL(String URL) {
         driver.get(URL);
     }
